2dc20aceb6683e7f6010daf12113c56325e5ee43
shiveshnavin <shiveshnavin@gmail.com>
2018-04-29 08:46:50 +0530

    Added Html and Handlebars

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 var express=require('express')
 var app=express()
+var hbs=require('express-handlebars')
+var path=require('path')
+
+app.engine('hbs',hbs({
+    extname:"hbs"
+}))
+
+
+app.set('view engine','hbs')
+app.use(express.static(path.join(__dirname,'public')))
 
 app.get('/',function(req,res){
 
 
-    res.send('Hello World !')
+    res.render('index',{
+
+        head:"Hello World !",
+        body:{
+            main_para_head:"Hello There !",
+            main_para:"This is an empty NodeJS and ExpressJS app with Handlebars . You can use it to quickly start building apps on top of it ."
+            
+        }
+
+    })
 
 
 })
